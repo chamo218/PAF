bb12be8ff7672e5ceea7e9e951958fdea5de07fa
DasunTM <it20166892@my.sliit.lk>
2022-04-25 21:45:57 +0530

    Second commit

--- src/main/java/model/Issue.java
+++ src/main/java/model/Issue.java
@@ -10,7 +10,7 @@ private Connection connect()
  Class.forName("com.mysql.jdbc.Driver");
 
  //Provide the correct details: DBServer/DBName, username, password
- con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/test", "root", "");
+ con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
  }
  catch (Exception e)
  {e.printStackTrace();}
